f02e983e3248e4c603dc4012c420eb737e1ea2dc
Raj Vaibhav Dubey <rajteknok@gmail.com>
2020-04-26 03:47:32 +0000

    process.env.port

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.use(express.static(__dirname + '/public'));
 app.get('*', function(req, res) {
     res.sendFile(`${__dirname}/public/index.html`);
 });
-server.listen(port, () => console.log(`Server is running on port ${port}`));
+server.listen(process.env.PORT || port, () => console.log(`Server is running on port ${port}`));
